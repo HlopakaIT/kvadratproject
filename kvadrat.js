3c83e8d6da8ad04e7ac484cccf0def3ce8b3532e
HlopakA <faka2@ukr.net>
2024-10-29 11:40:34 +0300

    ver 1.1?

--- kvadrat.js
+++ kvadrat.js
@@ -1,10 +1,19 @@
 const knopka = document.querySelector("input");
 
-
 knopka.onclick = createbox;
 
-function createbox() { 
-    const newbox = document.createElement("div");
-    newbox.className = "createbox";
-    document.body.append(newbox);
+function createbox() {
+    const deletebox = document.querySelector(".createbox");
+    const piizda = document.querySelector(".pizda")
+    if (deletebox) {
+        deletebox.remove(), piizda.remove();
+    } else {
+        const newbox = document.createElement("div");
+        newbox.className = "createbox";
+        document.body.append(newbox); 
+        const pizda = document.createElement("div");
+        pizda.className = "pizda";
+        pizda.innerHTML = "как же я заебался делать этот ебучий квадрат)"
+        document.body.append(pizda); 
+    }
 }
